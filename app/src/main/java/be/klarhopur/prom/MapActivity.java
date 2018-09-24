8d8d2098df4f784ec569828c7050b0cef98215c2
Unknown <blast.off2010@hotmail.com>
2018-09-24 12:01:05 +0200

    Fixed conflicts

--- app/src/main/java/be/klarhopur/prom/MapActivity.java
+++ app/src/main/java/be/klarhopur/prom/MapActivity.java
@@ -1,11 +1,13 @@
 package be.klarhopur.prom;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class MapActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle saveInstanceState){
-        
+        super.onCreate(saveInstanceState);
     }
 }
