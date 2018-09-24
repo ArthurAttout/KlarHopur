b9b285c3a0df86f8e28b7c4a4d446694246b8f55
Hendrik Debeuf <hendrik@hdinvest.xyz>
2018-09-24 14:55:17 +0200

    Layouts nouvelles promenades

--- app/src/main/java/be/klarhopur/prom/RandomPathActivity.java
+++ app/src/main/java/be/klarhopur/prom/RandomPathActivity.java
@@ -1,13 +1,37 @@
 package be.klarhopur.prom;
 
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class RandomPathActivity extends AppCompatActivity {
+public class RandomPathActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private BottomSheetBehavior mBottomSheetBehavior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_random_path);
+
+        View bottomSheet = findViewById( R.id.bottom_sheet );
+        FloatingActionButton button = (FloatingActionButton) findViewById( R.id.prom_floatingActionButton );
+
+
+        button.setOnClickListener(this);
+        mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        mBottomSheetBehavior.setPeekHeight(300);  // 0 if you don't want to show the bottom sheet on the starting activity.
+        mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.prom_floatingActionButton:
+                // run next activity
+                break;
+        }
     }
 }
