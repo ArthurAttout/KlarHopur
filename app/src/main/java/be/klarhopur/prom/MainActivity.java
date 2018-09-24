71654eafd1b11098487ffb75352973c55052544c
Malki <houda.malki.1993@gmail.com>
2018-09-24 11:18:41 +0200

    add distance_arrow 24 & 48

--- app/src/main/java/be/klarhopur/prom/MainActivity.java
+++ app/src/main/java/be/klarhopur/prom/MainActivity.java
@@ -1,5 +1,6 @@
 package be.klarhopur.prom;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
@@ -20,8 +21,11 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -38,6 +42,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG ="MainActivity";
+    private static  final int ERROR_DIALOG_REQUEST =9001;
 
     private RecyclerView recyclerView;
     private HistoryAdapter adapter;
@@ -60,8 +67,37 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new HistoryAdapter();
         recyclerView.setAdapter(adapter);
+
+        if(isServicesOK()) {
+        init();
+        }
     }
 
+    private void init(){
+
+    }
+    public boolean isServicesOK(){
+        Log.d(TAG,"isServicesOk: checking google services version");
+        int available= GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if(available== ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Log.d(TAG,"isServicesOk: google services is working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+
+            //an error occured but we can resolve it
+            Log.d(TAG,"isServicesOk: an error occured but we can fix it");
+            Dialog dialog= GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this,available,ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }
+        else {
+            Toast.makeText(this, "you can't make map", Toast.LENGTH_SHORT).show();
+        }
+
+        return false;
+    }
 
     private void openDialog() {
         View view = getLayoutInflater().inflate(R.layout.bottom_sheet_choose_path, null);
---
+++ app/src/main/java/be/klarhopur/prom/MapActivity.java
@@ -0,0 +1,11 @@
+package be.klarhopur.prom;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle saveInstanceState){
+        
+    }
+}
